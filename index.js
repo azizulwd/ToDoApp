a4839903a3c07f4f584820b35c623f7f561432de
azizulwd <azizulislamgd@gmail.com>
2021-08-23 03:08:47 +0600

    add task eventListener added

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 // Declare variables
-let addTask = document.getElementById("add_task");
+let addTaskBtn = document.getElementById("add_task");
 let addTaskInput = document.getElementById("add_task_input");
 let incompleteTask = document.getElementById("incomplete_task");
 let runningTask = document.getElementById("running_task");
 let completeTask = document.getElementById("complete_task");
 
-addTask.addEventListener("click", function(){
-    
+addTaskBtn.addEventListener("click", function(){
+    let createLi = document.createElement("li");
+    let createSpan = document.createElement("span");
+    let createButton = document.createElement("button");
+    createSpan.innerText = addTaskInput.value;
+    createButton.classList.add("running");
+    createButton.innerText = "Running";
+
+    // append child
+    incompleteTask.appendChild(createLi);
+    createLi.appendChild(createSpan);
+    createLi.appendChild(createButton);
+
+    // clear input
+    addTaskInput.value = "";
 });
