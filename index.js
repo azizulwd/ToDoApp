cf35cb46dda3bf9b1a28454a57f99a26843c5713
azizulwd <azizulislamgd@gmail.com>
2021-08-23 02:43:17 +0600

    declaring variables

---
+++ index.js
@@ -0,0 +1,10 @@
+// Declare variables
+let addTask = document.getElementById("add_task");
+let addTaskInput = document.getElementById("add_task_input");
+let incompleteTask = document.getElementById("incomplete_task");
+let runningTask = document.getElementById("running_task");
+let completeTask = document.getElementById("complete_task");
+
+addTask.addEventListener("click", function(){
+    
+});
